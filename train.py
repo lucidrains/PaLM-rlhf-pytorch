c4b2df49ae6f1f858721d4bae50a3351b4747495
Yacine Zahidi <yacine.zahidi@gmail.com>
2022-12-20 16:42:16 +0100

    Fix device bug
    Fix device bug and accelerator.print For Accelerator to work properly in distributed mode on only one process

--- train.py
+++ train.py
@@ -37,13 +37,17 @@ def decode_tokens(tokens):
     return "".join(list(map(decode_token, tokens)))
 
 
+# HuggingFace's Accelerator
+accelerator = Accelerator(gradient_accumulation_steps=GRADIENT_ACCUMULATE_EVERY)
+device = accelerator.device
+
 # instantiate GPT-like decoder model
 
 model = PaLM(
     num_tokens=256,
     dim=512,
     depth=8
-)
+).to(device)
 
 # prepare enwik8 data
 
@@ -62,7 +66,7 @@ class TextSamplerDataset(Dataset):
     def __getitem__(self, index):
         rand_start = torch.randint(0, self.data.size(0) - self.seq_len, (1,))
         full_seq = self.data[rand_start : rand_start + self.seq_len + 1].long()
-        return full_seq
+        return full_seq.to(device)
 
     def __len__(self):
         return self.data.size(0) // self.seq_len
@@ -76,10 +80,8 @@ val_loader = cycle(DataLoader(val_dataset, batch_size=BATCH_SIZE))
 
 optim = Adam(model.palm_parameters(), lr=LEARNING_RATE)
 
-accelerator = Accelerator(gradient_accumulation_steps=GRADIENT_ACCUMULATE_EVERY)
-
-model, optim, train_loader = accelerator.prepare(
-    model, optim, train_loader
+model, optim, train_loader, val_loader = accelerator.prepare(
+    model, optim, train_loader, val_loader
 )
 
 # training
@@ -102,14 +104,14 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10.0, desc="training"):
             model.eval()
             with torch.no_grad():
                 loss = model(next(val_loader), return_loss = True)
-                print(f"validation loss: {loss.item()}")
+                accelerator.print(f"validation loss: {loss.item()}")
 
         if i % GENERATE_EVERY == 0:
             model.eval()
             inp = random.choice(val_dataset)[:PRIME_LENGTH]
             prime = decode_tokens(inp)
-            print(f"%s \n\n %s", (prime, "*" * 100))
+            accelerator.print(f"%s \n\n %s", (prime, "*" * 100))
 
             sample = model.generate(GENERATE_LENGTH, inp[None, ...])
             output_str = decode_tokens(sample[0])
-            print(output_str, "\n")
+            accelerator.print(output_str, "\n")
