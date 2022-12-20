7458c2e1e32abb36ea4a0b748c14ed06c9bae37c
Yacine Zahidi <yacine.zahidi@gmail.com>
2022-12-20 15:35:00 +0100

    Fix .cuda() HfAccelerate conflict

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ model = PaLM(
     num_tokens=256,
     dim=512,
     depth=8
-).cuda()
+)
 
 # prepare enwik8 data
 
@@ -62,7 +62,7 @@ class TextSamplerDataset(Dataset):
     def __getitem__(self, index):
         rand_start = torch.randint(0, self.data.size(0) - self.seq_len, (1,))
         full_seq = self.data[rand_start : rand_start + self.seq_len + 1].long()
-        return full_seq.cuda()
+        return full_seq
 
     def __len__(self):
         return self.data.size(0) // self.seq_len
