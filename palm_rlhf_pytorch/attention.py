ad001d87525b4c54870d2ea54bf03ae45a62ba24
Phil Wang <lucidrains@gmail.com>
2023-03-17 16:54:33 -0700

    comment

--- palm_rlhf_pytorch/attention.py
+++ palm_rlhf_pytorch/attention.py
@@ -107,6 +107,14 @@ class Attention(nn.Module):
         return out
 
     def forward(self, q, k, v, mask = None):
+        """
+        einstein notation
+        b - batch
+        h - heads
+        n, i, j - sequence length (base sequence length, source, target)
+        d - feature dimension
+        """
+
         n, device = q.shape[-2], q.device
 
         scale = q.shape[-1] ** -0.5
