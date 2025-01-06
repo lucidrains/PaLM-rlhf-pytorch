2a9fdd409f28c7f96e10f0e53c70208d405494f7
Phil Wang <lucidrains@gmail.com>
2025-01-06 06:31:08 -0800

    export

--- palm_rlhf_pytorch/__init__.py
+++ palm_rlhf_pytorch/__init__.py
@@ -1,3 +1,5 @@
 from palm_rlhf_pytorch.palm import PaLM
-from palm_rlhf_pytorch.reward import RewardModel
 from palm_rlhf_pytorch.ppo import RLHFTrainer, ActorCritic
+
+from palm_rlhf_pytorch.reward import RewardModel
+from palm_rlhf_pytorch.implicit_process_reward import ImplicitPRM
